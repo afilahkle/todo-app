f97b4d1da2f3a7a2b8d6b2146210da278a84d08c
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-28 01:48:26 +0200

    test loading and error pages

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -5,6 +5,8 @@ import TodoList from '@components/TodoList';
 interface todosProps {}
 
 async function getData() {
+  // await new Promise((resolve) => setTimeout(() => resolve(), 2000)); // Test loadking page.
+  // await new Promise((resolve, reject) => setTimeout(() => reject(), 3000)); // Test error page.
   const todos = await db.todo.findMany({});
   console.log(todos);
   return todos;
