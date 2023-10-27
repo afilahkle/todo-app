5c5dfc63fbba94679b2e84a9681bc6a72e3e9d86
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-28 01:47:58 +0200

    create loading page

---
+++ app/(dashboard)/todos/loading.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { FC } from 'react';
+
+interface loadingProps {}
+
+const Loading: FC<loadingProps> = ({}) => {
+  return <div>loading....</div>;
+};
+
+export default Loading;
