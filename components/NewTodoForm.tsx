9e0d9b3b1733791fb99d4665155058661b7a3bac
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-28 01:49:23 +0200

    create a submit button

--- components/NewTodoForm.tsx
+++ components/NewTodoForm.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
+import { newTodo } from '@utils/actions';
 
 interface newTodoFormProps {}
 
 const NewTodoForm: FC<newTodoFormProps> = ({}) => {
   return (
-    <form>
-      <input type='text' className='border border-black/25'></input>
+    <form action={newTodo}>
+      <input
+        name='content'
+        type='text'
+        className='border border-black/25'
+      ></input>
+      <button type='submit'>New Todo</button>
     </form>
   );
 };
