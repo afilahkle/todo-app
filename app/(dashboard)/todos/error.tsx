054924d991b8b06660ea3123a63dfc08ecf4d132
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-28 01:47:46 +0200

    create an error page

---
+++ app/(dashboard)/todos/error.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React, { FC } from 'react';
+
+interface errorProps {}
+
+const ErrorUI: FC<errorProps> = ({}) => {
+  return <div>Ooops...</div>;
+};
+
+export default ErrorUI;
