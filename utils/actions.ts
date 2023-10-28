0433f13a324bc04970079bbce6b632bb0d4a066b
Omar Khalifa <elkhalifa002@gmail.com>
2023-10-28 07:10:38 +0200

    create a completeTodo action

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,6 +3,16 @@
 import { revalidatePath } from 'next/cache';
 import db from './db';
 
+export const completeTodo = async (id) => {
+  await db.todo.update({
+    where: { id },
+    data: {
+      completed: true,
+    },
+  });
+  revalidatePath('/todo');
+};
+
 export const newTodo = async (formData) => {
   const todo = await db.todo.create({
     data: {
